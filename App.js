39cb0fb5ec757b4af31874d1d84c3f61383ff8af
elcitrovmtgrande <victor.mahe.pro@gmail.com>
2022-10-10 18:25:46 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as openpgp from 'openpgp';
+
 
 export default function App() {
   return (
